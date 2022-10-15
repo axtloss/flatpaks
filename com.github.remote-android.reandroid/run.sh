1fbc7d441ac19848bbe8e5c9596d2fd8fb87c774
axtloss <axtlos@tar.black>
2022-10-15 15:25:59 +0200

    remove duplicate directory

--- com.github.remote-android.reandroid/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-if [[ ! -d /dev/binderfs ]]; then
-    /app/bin/setup-binder
-fi
-sudo podman run -itd --rm --privileged -v ~/data:/data -p 5555:5555 docker.io/redroid/redroid:13.0.0-amd64 androidboot.use_memfd=true
-adb connect localhost:5555 2>&1 && adb connect localhost:5555
-scrcpy -s localhost:5555 2>&1 && scrcpy -s localhost:5555
-
--- com.github.remote-android.reandroid/sudo.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-flatpak-spawn --host pkexec $@
